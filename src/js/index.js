47ecc288dacdd5ba68d582010281e6db202aac8f
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-06-02 15:18:53 -0700

    added on to searchView.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import {elements, renderLoader} from "./views/base";
 
 /* Global State of the app
     - Search Object
@@ -10,7 +12,8 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = "pizza" //TODO
+    const query = searchView.getInput();
+    console.log(query);
 
     //If query is not empty
     if(query) {
@@ -18,21 +21,21 @@ const controlSearch = async () => {
         state.search = new Search(query);
 
         // 3) Prep UI
-            //TODO
-        
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
         // 4) Search for the recipes
         await state.search.getResults(); //Await because the results of getResults is a promise and will need fetch from the server first. EVERY ASYNC FUNCTION returns a promise
 
         // 5) Render result on the UI
-        console.log(state.search.result);
-    } 
-
+        //console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+    }
 }
 
-document.querySelector(".search").addEventListener("submit", event => {
+elements.searchForm.addEventListener("submit", event => {
     event.preventDefault();
     controlSearch();
+
 });
-
-
-
